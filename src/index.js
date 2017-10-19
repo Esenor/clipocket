fbeeed48032825e3121a958e1fd0ca9becdebac0
David Dieu <daviddieu80@gmail.com>
2017-10-19 12:50:24 +0200

    handle last clip

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ const http = require('http').Server(app)
 const path = require('path')
 const io = require('socket.io')(http)
 
+var lastClip = ''
+
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, './template/index.html'))
 })
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+  io.emit('clip_update', lastClip)
   socket.on('disconnect', () => {
     console.log('Client disconnected')
   })
   socket.on('clip_update', (clip) => {
     console.log('Clip receive', (new Date().toString()))
-    io.emit('clip_update', clip)
+    lastClip = clip
+    io.emit('clip_update', lastClip)
   })
 })
 
